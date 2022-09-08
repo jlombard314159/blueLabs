04213047a075eea5883495c2b904bc419434b530
Hilary Brennan <hilary.brennan@bluelabs.com>
2022-09-08 17:21:10 -0400

    finished debugging show_stock.sh

--- inventory_manager/show_stock.sh
+++ inventory_manager/show_stock.sh
@@ -8,33 +8,35 @@ if ! grep -q "^${1}" "inventory.csv"; then
     exit 0
 fi
 
-STYLE_ROWS=($(grep "^${1},null" 'inventory.csv'))
+STYLE_ROWS=$(grep "${1},null" 'inventory.csv')
+STYLE_COUNT=$(grep "${1},null" -o 'inventory.csv' | wc -l)
 
-if [ ${#STYLE_ROWS[@]} == 0 ]; then
+if [ $STYLE_COUNT == 0 ]; then
     echo "There are no units of Style #${1} currently in stock."
     exit 0
 fi
-echo $STYLE_ROWS
-COLOR_ROWS=($(echo $STYLE_ROWS | grep ",${2},"))
-echo $COLOR_ROWS
 
-if [ ${#COLOR_ROWS[@]} == 0 ]; then
+COLOR_ROWS=$(echo $STYLE_ROWS | grep ",${2},")
+COLOR_COUNT=$(echo $STYLE_ROWS | grep -o ",${2}," | wc -l)
+
+if [ $COLOR_COUNT == 0 ]; then
     echo "There are no ${2} units of Style #${1} currently in stock."
     exit 0
 fi
 
-XS_STOCK = ($(echo $COLOR_ROWS | grep ",XS,"))
-S_STOCK = ($(echo $COLOR_ROWS | grep ",S,"))
-M_STOCK = ($(echo $COLOR_ROWS | grep ",M,"))
-L_STOCK = ($(echo $COLOR_ROWS | grep ",L,"))
-XL_STOCK = ($(echo $COLOR_ROWS | grep ",XL,"))
+XS_STOCK=$(echo $COLOR_ROWS | grep -o ",XS," | wc -l)
+S_STOCK=$(echo $COLOR_ROWS | grep -o ",S," | wc -l)
+M_STOCK=$(echo $COLOR_ROWS | grep -o ",M," | wc -l)
+L_STOCK=$(echo $COLOR_ROWS | grep -o ",L," | wc -l)
+XL_STOCK=$(echo $COLOR_ROWS | grep -o ",XL," | wc -l)
 
+echo "Style: ${1} - Color: ${2}"
 echo "Current Stock: "
-echo "XS: ${#XS_STOCK[@]}"
-echo "S: ${#S_STOCK[@]}"
-echo "M: ${#M_STOCK[@]}"
-echo "L: ${#L_STOCK[@]}"
-echo "XL: ${#XL_STOCK[@]}"
+echo "XS: $XS_STOCK"
+echo "S: $S_STOCK"
+echo "M: $M_STOCK"
+echo "L: $L_STOCK"
+echo "XL: $XL_STOCK"
 
 
 
